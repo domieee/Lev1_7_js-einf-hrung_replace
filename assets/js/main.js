408bebdd00e721adcd39cc666113f919fb667bb7
domieee <gartzdominik@gmail.com>
2023-01-24 12:30:09 +0100

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const text = "Sam is good at coding school";
+
+const firstResult = text.replace("good", "bad");
+const secondResult = text.replace("Sam", "Susi");
+const thirdResult = text.replace("coding school", "Tennis");
+
+document.write(firstResult + "<br>" + secondResult + "<br>" + thirdResult);
